a628c2dda8c2a6b60d5675e4234ff528de5c1616
rlzb <kalyeltonvieira@gmail.com>
2025-07-18 15:56:18 -0300

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -1,3 +1,7 @@
 print("Hello, world")
 nome = str(input("digite seu nome: "))
+if nome == "joao":
+    print(f"que nome legal {nome}")
+else:
+    print("seu nome e normal")
 print(f"bem vindo {nome}")
