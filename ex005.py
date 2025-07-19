506586422b2f99126cf7e104ef60d3f336a4f5ed
rlzb <kalyeltonvieira@gmail.com>
2025-07-19 18:55:02 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -1,9 +1,9 @@
 n1 = int(input('digite um valor: '))
 n2 = int(input('digite outro valor: '))
-s = n1 + n2
-m = n1 * n2
-d = n1 / n2
-di = n1 // n2
-e = n1 ** n2
+s = (n1 + n2)
+m = (n1 * n2)
+d = (n1 / n2)
+di = (n1 // n2)
+e = (n1 ** n2)
 print(f'A soma e {d} o produto e {m} e a divisao e {di:.3f}')
-print('Divisao inteira {di} e potencia {e}')
+print(f'Divisao inteira {di} e potencia {e}')
