1d35b345af9fe6cf09de57be9fcf24050c155b07
rlzb <kalyeltonvieira@gmail.com>
2025-07-20 20:30:30 -0300

    Update ex0010.py

--- ex0010.py
+++ ex0010.py
@@ -1,3 +1,4 @@
-dinheiro = float(input("Quantos Reais voce tem na Carteira? R$"))
-dolar = dinheiro / 5.58
-print(f"Convertendo R${dinheiro}, Para USD voce teria US${dolar:.2f}")
+while True:
+    real = float(input("Quantos Reais voce tem na Carteira? R$"))
+    dolar = real / 5.58
+    print(f"Convertendo R${real}, Para USD voce teria US${dolar:.2f}")
