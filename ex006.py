653ca3b1eaae88318a1464fd2a107fba95782564
rlzb <kalyeltonvieira@gmail.com>
2025-07-20 15:47:20 -0300

    Update ex006.py

---
+++ ex006.py
@@ -0,0 +1,6 @@
+n = int(input("Digite um Numero: ")) #variavel n
+d = (n *2)
+t = (n * 3)
+r = (n ** (1/2))
+print(f"O Dobro de {n} vale {d}")
+print(f"O Triplo de {n} vale {t} \nA raiz Quadrada de {n} e Igual a {r:.2f}")
