8eb75f7f50455a72bfe048bb513b2de70aed7d73
rlzb <kalyeltonvieira@gmail.com>
2025-07-20 15:47:25 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -1,4 +1,4 @@
 numero = int(input("digite um numero: "))
-s = (numero +1)
-a = (numero -1)
-print(f"o Antecessor e {a} e o sucessor e {s}")
+#s = (numero +1)
+# a = (numero -1)
+print(f"o Antecessor e {numero -1} e o sucessor e {numero +1}")
