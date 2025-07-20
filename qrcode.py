0a039e85d98e291d1df55084e8f1989701651c82
rlzb <kalyeltonvieira@gmail.com>
2025-07-19 23:08:43 -0300

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -6,15 +6,15 @@ senha = "SenhaSecreta"
 tipo_seguranca = "WPA"
 
 #formato  para QR CODE WI-FI:
-#WI-FI:T:<tipo_seguranca>;S:{ssid};P:{senha};;
+#WIFI:T:<tipo_seguranca>;S:{ssid};P:{senha};;
 
 
-wifi_qr = f"WIFI:T:{tipo_seguranca};S:{ssid}:P{senha};;"
+wifi_qr = f"WIFI:T:{tipo_seguranca};S:{ssid};P:{senha};;"
 
 #Gerar QR CODE
 qr = qrcode.make(wifi_qr)
 
 #Salvar Imagem
-qr.save("Wi-Fi_qr_code.pgn")
+qr.save("wifiqrcode.png")
 
 print("QR CODE CRIADO E SALVO COMO Wi-Fi_qr_code.png")
