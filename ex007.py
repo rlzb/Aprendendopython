f1e3069208a850cf56b07a97581bf79f8ec64b80
rlzb <kalyeltonvieira@gmail.com>
2025-07-20 16:01:00 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,4 @@
+nota1 = float(input("Digite A Primeira Nota: "))
+nota2 = float(input("DIgite A Segunda Nota: "))
+media = (nota1 + nota2) /2
+print(f"A Media Entre {nota1} e {nota2} sera {media:.1f}")
