acd82f782819f99baa3171c4ee0cc8feb15dfadc
rlzb <kalyeltonvieira@gmail.com>
2025-07-21 11:13:05 -0300

    Update ex0012.py

--- ex0012.py
+++ ex0012.py
@@ -1,3 +1,3 @@
 preco = float(input("Digite o Valor para obter o desconto? R$"))
 novo = preco - (preco * 5 /100)
-print(f"o produto que custava R${preco}, Com Desconto de 5% vai custar R${novo}")
+print(f"o produto que custava R${preco:.2f}, Com Desconto de 5% vai custar R${novo}")
