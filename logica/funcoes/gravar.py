5404863e6a695fdfcb90f803aec6590f33c13e8d
rlzb <kalyeltonvieira@gmail.com>
2025-07-21 12:51:36 -0300

    Update gravar.py

--- logica/funcoes/gravar.py
+++ logica/funcoes/gravar.py
@@ -3,3 +3,8 @@ arquivo = open('arqtexto.txt', 'w')
 arquivo.write('Curso Python \n')
 arquivo.write('Aula Pratica')
 arquivo.close()
+
+#Leitura do arquivo texto
+leitura = open('arqtexto.txt', 'r')
+print(leitura.read())
+leitura.close()
